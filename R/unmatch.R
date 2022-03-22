368f9f54842cfbf6cfb3688d637b52677af0e09f
jasenfinch <jsf9@aber.ac.uk>
2022-03-22 17:41:40 +0000

    `%!in%` documentation fixes

--- R/unmatch.R
+++ R/unmatch.R
@@ -1,9 +1,9 @@
 #' @name unmatch
 #' @rdname unmatch
-#' @title Unmatching operator
-#' @description A binary operator to detect unmatching values. The opposite of the `%in%` operator. 
-#' @param x vector or NULL: the values to be unmatched
-#' @param y vector or NULL: the values to be unmatched against
+#' @title Unmatched operator
+#' @description A binary operator to detect unmatched values. The opposite of the `%in%` operator. 
+#' @param x vector or NULL: the values to be compared
+#' @param y vector or NULL: the values to be compared against
 #' @examples 
 #' 1:10 %!in% c(1,3,5,9)
 #' @export
