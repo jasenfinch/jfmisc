6d786c4f3fd5d22e8d06ff2d3e53d8cdd64f6727
jasenfinch <jsf9@aber.ac.uk>
2022-09-06 19:54:19 +0100

    rexport ptol_pal, scale_colour_ptol and scale_fill_ptol from ggthemes

--- R/reexports.R
+++ R/reexports.R
@@ -2,3 +2,18 @@
 #' @rdname reexports
 #' @export
 magrittr::`%>%`
+
+#' @importFrom ggthemes ptol_pal
+#' @rdname reexports
+#' @export
+ggthemes::ptol_pal
+
+#' @importFrom ggthemes scale_colour_ptol
+#' @rdname reexports
+#' @export
+ggthemes::scale_colour_ptol
+
+#' @importFrom ggthemes scale_fill_ptol
+#' @rdname reexports
+#' @export
+ggthemes::scale_fill_ptol
