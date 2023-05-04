3585ebf638df47c669e6b1da13bf36b3c6961965
jasenfinch <jsf9@aber.ac.uk>
2023-05-04 15:41:36 +0100

    Added suitableParallelStrategy() and suitableParallelWorkers()

--- R/parallel.R
+++ R/parallel.R
@@ -1,46 +1,73 @@
 #' Generate a suitable parallel processing plan
-#' @description Generate a suitable parallel processing future plan for your session. See `?future::plan` for more details on future plans.
-#' @param strategy The parallel strategy to use. See `?future::plan` for details. If `NULL` (the default), an appropriate strategy will be detected.
-#' @param workers The number of workers to use. If `NULL`, the arguments `RAM_per_worker` and `proportion_max_workers` will be used to calculate a suitable number of workers.
-#' @param RAM_per_worker The memory (in gigabytes) to allocate to each worker when calculating a suitable number of workers. Ignored if argument `workers` is not `NULL`.
-#' @param proportion_max_workers A value between 0 and 1. The maximum number of workers to allocate as a proportion of the total number of workers available (as detected by `future::availableCores()`). Ignored if argument `workers` is not `NULL`.
+#' @rdname parallel
+#' @description Generate a suitable parallel processing future plan for your session. See `?future::plan` 
+#' for more details on future plans.
+#' @param strategy The parallel strategy to use. See `?future::plan` for details.
+#' @param workers The number of workers to use.
+#' @param RAM_per_worker The memory (in gigabytes) to allocate to each worker when calculating a suitable 
+#' number of workers. 
+#' @param proportion_max_workers A value between 0 and 1. The maximum number of workers to allocate as a 
+#' proportion of the total number of workers available (as detected by `future::availableCores()`). 
+#' @param ... Arguments to pass to `future::plan()`.
 #' @examples 
+#' ## Return a suitable parallel strategy for the current session
+#' suitableParallelStrategy()
+#' 
+#' ## Return a suitable number parallel workers based on the system resources.
+#' suitableParallelWorkers()
+#' 
 #' \dontrun{
+#' ## Generate a suitable parallel plan
 #' suitableParallelPlan()
 #' }
 #' @importFrom future availableCores multisession multicore plan supportsMulticore
 #' @importFrom memuse Sys.meminfo
 #' @export
 
-suitableParallelPlan <- function(strategy = NULL,workers = NULL,RAM_per_worker = 8,proportion_max_workers = 0.75){
+suitableParallelPlan <- function(strategy = suitableParallelStrategy(),
+                                 workers = suitableParallelWorkers(),
+                                 ...){
   
-  if (is.null(strategy)) strategy <- ifelse(
+  plan(strategy,workers = workers,...)
+  
+  if (!is.character(strategy)) strategy <- class(strategy)[1]
+  
+  message(paste('Setting up a',strategy,'plan with',workers,'workers'))
+}
+
+#' @rdname parallel
+#' @export
+
+suitableParallelStrategy <- function(){
+  strategy <- ifelse(
     supportsMulticore(),
     'multicore',
     'multisession')
   
-  if (is.null(workers)) {
-    total_RAM <- Sys.meminfo()[['totalram']] %>% 
-      gsub('[a-zA-Z]','',x = .) %>% 
-      as.numeric()
-    
-    suitable_workers <- floor(total_RAM / RAM_per_worker)
-    max_workers <- ceiling(availableCores() * proportion_max_workers)
-    
-    if (suitable_workers > max_workers) {
-      workers <- max_workers 
-    } else {
-      workers <- suitable_workers
-    }
+  strategy
+}
+
+#' @rdname parallel
+#' @export
+
+suitableParallelWorkers <- function(RAM_per_worker = 8,proportion_max_workers = 0.75){
+  
+  total_RAM <- Sys.meminfo()[['totalram']] %>% 
+    gsub('[a-zA-Z]','',x = .) %>% 
+    as.numeric()
+  
+  suitable_workers <- floor(total_RAM / RAM_per_worker)
+  max_workers <- ceiling(availableCores() * proportion_max_workers)
+  
+  if (suitable_workers > max_workers) {
+    workers <- max_workers 
+  } else {
+    workers <- suitable_workers
   }
   
   if (workers < 1){
     workers <- 1
   }
   
-  plan(strategy,workers = workers)
-  
-  if (!is.character(strategy)) strategy <- class(strategy)[1]
-  
-  message(paste('Setting up a',strategy,'plan with',workers,'workers'))
+  return(workers)
 }
