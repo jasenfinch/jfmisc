33f6ec5f0dd9d6fb58f520081d61b0236da96496
jasenfinch <jsf9@aber.ac.uk>
2024-02-20 13:28:30 +0000

    fix reexports documentation

--- R/reexports.R
+++ R/reexports.R
@@ -1,19 +1,15 @@
 #' @importFrom magrittr %>%
-#' @rdname reexports
 #' @export
 magrittr::`%>%`
 
 #' @importFrom ggthemes ptol_pal
-#' @rdname reexports
 #' @export
 ggthemes::ptol_pal
 
 #' @importFrom ggthemes scale_colour_ptol
-#' @rdname reexports
 #' @export
 ggthemes::scale_colour_ptol
 
 #' @importFrom ggthemes scale_fill_ptol
-#' @rdname reexports
 #' @export
 ggthemes::scale_fill_ptol
